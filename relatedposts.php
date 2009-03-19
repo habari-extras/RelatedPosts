6c0e95a4114cf05f0be6f88641a86fe0117cf1b1
eighty4 <USER@DOMAIN>
2009-03-19 19:37:26 +0000

    adding RelatedPosts files
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/RelatedPosts/trunk@1716 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ relatedposts.php
@@ -0,0 +1,10 @@
+<ul class="related-post">
+<?php 
+	$li_class= 'related-post-odd';
+	foreach( $theme->related_entry as $post ) {
+		$li_class= ( $li_class == 'related-post-even' ? 'related-post-odd' : 'related-post-even' );
+		echo '<li class="' . $li_class . '">' . '<a href="' . $post->permalink . '">' . $post->title . '<small>' .
+		$post->pubdate->get('l, F jS, Y') . '</small></a>' . "</li>\n";
+} 
+?>
+</ul>
