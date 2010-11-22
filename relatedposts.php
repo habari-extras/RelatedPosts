1aff5acfb9be815e33389c06e2655feb255d54a3
rickc <USER@DOMAIN>
2010-11-22 04:42:01 +0000

    Plugin: RelatedPosts. Made Habari 0.7 compatible. Simplified plugin. Added help text. Added a header for the list of related posts. This should really have a block.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/RelatedPosts/trunk@3533 f4a90942-1786-4aa1-b092-57c31935ddc3

--- relatedposts.php
+++ relatedposts.php
@@ -1,10 +1,10 @@
+<?php echo $rl_header; ?>
 <ul class="related-post">
-<?php 
+<?php
 	$li_class= 'related-post-odd';
 	foreach( $theme->related_entry as $post ) {
 		$li_class= ( $li_class == 'related-post-even' ? 'related-post-odd' : 'related-post-even' );
-		echo '<li class="' . $li_class . '">' . '<a href="' . $post->permalink . '">' . $post->title . '<small>' .
-		$post->pubdate->get('l, F jS, Y') . '</small></a>' . "</li>\n";
+		echo "<li class=\"{$li_class}\"><a href=\"{$post->permalink}\">{$post->title}<small> {$post->pubdate->get('l, F jS, Y')}</small></a></li>\n";
 } 
 ?>
 </ul>
